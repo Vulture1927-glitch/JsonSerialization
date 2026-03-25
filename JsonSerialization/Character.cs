2f48fce1276a134bc021026f6e7768d1b0485a3f
Isaac Stumm <isaac.stumm@students.snow.edu>
2026-03-25 16:18:47 -0600

    Add project files.

---
+++ JsonSerialization/Character.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    internal class Character
+    {
+        public string Name { get; set; }
+        public string ClassType { get; set; }
+        public int Level { get; set; }
+        public int Health { get; set; }
+        public int Mana { get; set; }
+    }
+}
---
+++ JsonSerialization/Program.cs
@@ -0,0 +1,34 @@
+﻿using Classes;
+using System.Text.Json;
+public class Program
+{
+    static void Main()
+    {
+        Character hero = new Character()
+        {
+            Name = "The Man",
+            ClassType = "Warrior",
+            Level = 5,
+            Health = 120,
+            Mana = 10,
+        };
+
+        string path = @"C:\Users\isaac.stumm\source\repos\JsonSerialization\JsonSerialization\Data.json";
+        var options = new JsonSerializerOptions
+        {
+            WriteIndented = true,
+        };
+        string convert = JsonSerializer.Serialize(hero, options);
+        File.WriteAllText(path, convert);
+        Console.WriteLine(convert);
+
+        string text = File.ReadAllText(path);
+        Character theReturn = JsonSerializer.Deserialize<Character>(text);
+        Console.WriteLine(theReturn.Name);
+        Console.WriteLine(theReturn.ClassType);
+        Console.WriteLine(theReturn.Level);
+        Console.WriteLine(theReturn.Health);
+        Console.WriteLine(theReturn.Mana);
+
+    }
+}
